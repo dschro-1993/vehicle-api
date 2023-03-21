75270fb0d354bd08a26ae58fc3cb800885df3ce8
Schroettinger, Dominik <dominik.schroettinger@devoteam.com>
2023-03-21 23:57:29 +0100

    minor fixes

--- terraform/qa/main.tf
+++ terraform/qa/main.tf
@@ -1,9 +1,9 @@
 module "vehicle_api" {
   source = "git::https://github.com/dschro-1993/vehicle-api-terraform-module.git?ref=0.1.0"
 
-  openapi_spec_path = "${path.module}/../../openapi.yml"
-  code_path         = "${path.module}/../../vehicle_api"
   zone_name         = "292372118261.starfish-rentals.com"
+  code_path         = "${path.module}/../../vehicle_api"
+  openapi_spec_path = "${path.module}/../../openapi.yml"
   app_handler       = "api_resolver.lambda_handler"
   app_version       = var.app_version
   env               = "qa"
