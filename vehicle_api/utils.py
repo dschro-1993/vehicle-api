b11c9a63171e3bf4e545ab940b6862536a0a6a68
Dominik Schroettinger <dominik.schroettinger@devoteam.com>
2024-09-16 13:57:28 +0200

    .

--- vehicle_api/utils.py
+++ vehicle_api/utils.py
@@ -1,13 +1,3 @@
 """Utilities for Vehicle-API"""
 
-from botocore.config import Config
-
-boto_config = Config(
-  # proxies = {...},
-  # proxies_config = {...},
-  # retries = {"mode": "standard"},
-  tcp_keepalive = True,
-  # {...}
-)
-
 # {...}
